803d08b56d769bd4b88f7996d7e1a15b788d603a
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-30 14:53:18 -0700

    add todo create and destroy functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,12 @@
  */
 angular.module('yotodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = [];
+    $scope.addToDo = function(){
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    $scope.removeToDo = function(index){
+      $scope.todos.splice(index,1);
+    };
   });
